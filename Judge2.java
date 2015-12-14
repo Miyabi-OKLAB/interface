94e4c00cdaccb7f74e17325a2561c9840c3336eb
shuheikawai <for.my.university@gmail.com>
2015-12-14 12:51:01 +0900

    関数化したJudge.javaの作成
    オブジェクトっぽい書き方にするため外部関数ライクに使えるJudge。
    戻り値はJPanelに載せるのが目標なのでStaticです。

---
+++ Judge2.java
@@ -0,0 +1,81 @@
+//文字の読み込みと標準出力を行う
+//int -> char -> String と型を渡り歩いてるクソコード
+//created 1.0 bykay at 2015.12.06
+//improve 1.3 bykay at 2015.12.14
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Judge2{
+  //public static void main(String[] args){
+  public static String FuncJudge2(){
+    int i = 0;
+    int j = 0;
+    int count = 0;
+    int[] arr = new int[80];
+    int[] arr2 = new int[100];
+//staticは構造体の宣言。中に変数がある場合はstaticを置く。
+
+  try{
+    File file = new File("./No_one.txt");
+    FileReader filereader = new FileReader(file);
+
+  do{
+    arr[i] = filereader.read() ;
+    i++;
+    }while(arr[i-1]!=-1);//read number of "0"
+
+  for(i = 0;i < arr.length-1;i++){
+    if (arr[i]==49){
+    break;
+    }
+  count++;
+  }
+
+  filereader.close();
+
+  }catch(FileNotFoundException e){
+  System.out.println(e);
+  }catch(IOException e){
+  System.out.println(e);
+  }
+
+
+  try{
+    File file = new File("./DataBase.txt");
+    FileReader filereader = new FileReader(file);
+
+    do{
+      arr2[j] = filereader.read() ;
+      j++;
+    }while(arr2[j-1]!=-1);//read text of file.
+
+   j = j-1;
+   j = count;
+
+  //System.out.println(arr2[count]);
+  //System.out.println((char)arr2[count]);
+   filereader.close();
+
+   }catch(FileNotFoundException e){
+   System.out.println(e);
+   }catch(IOException e){
+   System.out.println(e);
+ }//catch
+
+//char test = '12354';         //unicode(10進)
+//System.out.println(test);
+//String hiragana = "\u3042";//
+//System.out.println(hiragana);
+
+String str = String.valueOf((char)arr2[count]);
+System.out.println(str);
+
+
+return str;
+
+ }//main
+}//class
